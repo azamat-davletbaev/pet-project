7c48c1603c3add50512791dff0b1106be052209f
Azamat Davletbaev <azamat.davletbaev@gmail.com>
2024-03-30 12:57:44 +0500

    2

---
+++ PetProject/PetProject/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new PostgresContext();
+
+            var x1 = ctx.USERS.ToList();
+            var x2 = ctx.Expenses.ToList();
+            var x3 = ctx.CachAccounts.ToList();
+            var x4 = ctx.CashExpenses.ToList();
+
+        }
+    }
+}
---
+++ PetProject/PetProject/Tables/Expenses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Tables
+{
+    public class Expenses
+    {
+        [Column(TypeName = "ID")]
+        public int ID { get; set; }
+
+        [Column(TypeName = "Name")]
+        public string Name { get; set; }
+    }
+}
