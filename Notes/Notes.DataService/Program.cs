55a5cbc00286db4535e999219288f4a86871d643
Azamat Davletbaev <azamat.davletbaev@gmail.com>
2024-06-15 17:45:53 +0500

    queue setting

--- Notes/Notes.DataService/Program.cs
+++ Notes/Notes.DataService/Program.cs
@@ -1,22 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Notes.DataService.Queue;
-using Notes.DI;
 using Notes.Model;
 
 namespace Notes.DataService
 {
     internal class Program
     {
-        public static IServiceProvider ServiceProvider = DI.Services.ServiceProvider;
-        private static readonly string MainQueue = "myQueue";
+        public static IServiceProvider ServiceProvider = DI.Services.ServiceProvider;        
         static void Main(string[] args)
         {
             using (var ctx = new PostgresContext())
             {
                 var serviceCollection = new ServiceCollection();
                 serviceCollection.AddSingleton<IDataProvider>(z => new DataProvider(ctx));
-                serviceCollection.AddSingleton<IQueueSender>(z=> new QueueSender(queueName: MainQueue, hostName: "localhost", userName: "guest", password: "guest"));
-                serviceCollection.AddSingleton<IQueueReceiver>(z => new QueueReceiver(queueName: MainQueue, hostName: "localhost", userName: "guest", password: "guest"));
+                serviceCollection.AddSingleton<IQueueSender>(z=> new QueueSender(QueueSettings.QueueName, QueueSettings.HostName, QueueSettings.UserName, QueueSettings.Password));
+                serviceCollection.AddSingleton<IQueueReceiver>(z => new QueueReceiver(QueueSettings.QueueName, QueueSettings.HostName, QueueSettings.UserName, QueueSettings.Password));
 
                 ServiceProvider = serviceCollection.BuildServiceProvider();
 
@@ -24,12 +22,12 @@ namespace Notes.DataService
                 //var users = db.GetAllUsers();
 
                 var q = ServiceProvider.GetRequiredService<IQueueSender>();                
-                q.SendMessage($"({DateTime.Now}): Hello !!!");
+                q.SendMessage($"Hello !!!");
 
                 var receiver = ServiceProvider.GetRequiredService<IQueueReceiver>();
                 receiver.OnReceivered += (message) =>
                 {
-                    Console.WriteLine($"Получено в {DateTime.Now}: {message}");
+                    Console.WriteLine($"Получено: {message}");
                 };
                                 
                 Console.ReadKey();
---
+++ Notes/Notes.Model/QueueSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Model
+{
+    public static class QueueSettings
+    {
+        public static readonly string QueueName = "NotesQueue";
+        public static readonly string HostName = "localhost";
+        public static readonly string UserName = "guest";
+        public static readonly string Password = "guest";
+    }
+}
